f705ab13955affd64f8c87f71fd5212b295c6823
Abel <persistentabel@gmail.com>
2025-01-03 17:26:11 -0800

    [Patch] Fix Click Me background color to red (was green)

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
           Button("Click me") {
             
           }
-          .background(Color.green)
+          .background(Color.red)
 
         }
         .padding()
