f89d4a1d16b3bb9c974614c36edbd374dff4e46e
Abel <persistentabel@gmail.com>
2025-01-03 17:09:57 -0800

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Abel Gomez on 1/3/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen Two")
+    }
+}
+
+#Preview {
+    HomeView()
+}
